c54d26d97c0bd48456407c9b233f5ccebbad7445
Michael Kamenev <dlyapicem@gmail.com>
2021-11-12 05:36:30 +0300

    пользователи не получают общий доступ к данным приложения

--- src/psychics/models.py
+++ src/psychics/models.py
@@ -1,7 +1,3 @@
-import random
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
 from pynames.generators.russian import PaganNamesGenerator
 import random
 
@@ -9,51 +5,45 @@ import random
 class Psychics:
     def __init__(self):
         self.name = PaganNamesGenerator().get_name_simple()
-        self.trust_level = 0
-        self.psy_num = 0
-        self.all_number = []
+        self.trust_level = {}
+        self.psy_num = {}
+        self.all_number = {}
 
-    def make_choice(self):
-        """Делаем попытку отгадать число, загаданное пользователем"""
-        self.psy_num = random.randint(10, 99)
-        self.all_number.append(self.psy_num)
+    def make_choice(self, username):
+        self.psy_num[username] = random.randint(10, 99)
 
-    def check_answer(self, user_answer):
-        """Проверка правильности данного ответа"""
-        if user_answer == self.psy_num:
-            self.trust_level += 1
+    def get_rating(self, username):
+        return self.trust_level.setdefault(username, 0)
+
+    def change_rating(self, username, increase_number):
+        self.trust_level[username] = self.get_rating(username) + increase_number
+
+    def check_answer(self, user_answer, username):
+        all_number = self.all_number.setdefault(username, [])
+        all_number.append({'psy_num': self.psy_num[username]})
+        if user_answer == self.psy_num[username]:
+            self.change_rating(username, 1)
         else:
-            self.trust_level -= 1
+            self.change_rating(username, -1)
 
 
 class PsyController:
-    """Class represents psychics controller
-    """
-
     def __init__(self):
-        """ Initializer
-        """
         self.psychics = [Psychics() for i in range(2, 6)]
 
-    def get_choices(self):
-        """ Get all psychics guesses
-        """
+    def get_choices(self, username):
         return [{'psy_name': psychic.name,
-                 'psy_num': psychic.make_choice()}
+                 'psy_num': psychic.make_choice(username)}
                 for psychic in self.psychics]
 
-    def check_all(self, number):
-        """ Check all psychics guesses
-        """
+    def check_all(self, number, username):
         for psychics in self.psychics:
-            psychics.make_choice()
-            psychics.check_answer(number)
-        return self.get_all()
+            psychics.make_choice(username)
+            psychics.check_answer(number, username)
+        return self.get_all(username)
 
-    def get_all(self):
-        """ Returns list of dict of all psychics
-        """
+    def get_all(self, username):
         return [{'psy_name': psychic.name,
-                 'psy_all_number': psychic.all_number,
-                 'psy_trust_level': psychic.trust_level}
+                 'psy_all_number': psychic.all_number.get(username),
+                 'psy_trust_level': psychic.get_rating(username)}
                 for psychic in self.psychics]
